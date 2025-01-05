71b1f7f939f36a55e55224599f6c9b8b37341988
murali.rajendran <murali.rajendran@veeva.com>
2025-01-04 23:14:58 -0500

    Modified terraform key

--- eks/backend.tf
+++ eks/backend.tf
@@ -2,19 +2,19 @@ terraform {
   required_version = ">=1.10.3"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.82.2"
     }
   }
   backend "s3" {
     bucket         = "use1-remote-terraform-state-file-bucket"
     region         = "us-east-1"
-    key            = "Project-2-Three-Tier-DevSecOps-Pipeline-Lock-Files/EKS/terraform.tfstate"
+    key            = "EKS/terraform.tfstate"
     dynamodb_table = "Project-2-Three-Tier-DevSecOps-Pipeline-Lock-Files-EKS"
     encrypt        = true
   }
 }
 
 provider "aws" {
-  region  = var.aws-region
+  region = var.aws-region
 }
